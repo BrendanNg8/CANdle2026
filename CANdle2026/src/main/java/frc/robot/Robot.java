e01f07b5eab01c6a3d870539bbbf258632b2ebf5
Brendan Ng <brendanng88@gmail.com>
2026-03-04 19:59:47 -0500

    ---

---
+++ CANdle2026/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,168 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import com.ctre.phoenix6.configs.CANdleConfiguration;
+import com.ctre.phoenix6.controls.*;
+import com.ctre.phoenix6.hardware.CANdle;
+import com.ctre.phoenix6.signals.RGBWColor;
+import com.ctre.phoenix6.signals.StatusLedWhenActiveValue;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
+/**
+ * The methods in this class are called automatically corresponding to each mode, as described in
+ * the TimedRobot documentation. If you change the name of this class or the package after creating
+ * this project, you must also update the Main.java file in the project.
+ */
+public class Robot extends TimedRobot {
+  private Command m_autonomousCommand;
+
+  private final RobotContainer m_robotContainer;
+  private final CANdle candle = new CANdle(4, "rio"); //Adjust ID
+  private static final RGBWColor kBlue = RGBWColor.fromHex("#04D9FF").orElseThrow();
+  private static final RGBWColor kRed = RGBWColor.fromHex("#FC1723").orElseThrow();
+
+  private AnimationType state1 = AnimationType.None;
+
+  private final SendableChooser<AnimationType> ledModeChooser1 = new SendableChooser<AnimationType>();
+  
+  public enum AnimationType {
+        None,
+        Rainbow,
+        SolidBlue,
+        SolidRed
+  }
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  public Robot() {
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
+    var cfg = new CANdleConfiguration();
+    cfg.LED.BrightnessScalar = 0.5;
+    cfg.CANdleFeatures.StatusLedWhenActive = StatusLedWhenActiveValue.Disabled; // Turn off status LED
+    candle.getConfigurator().apply(cfg);
+
+
+    ledModeChooser1.setDefaultOption("None", AnimationType.None);
+    ledModeChooser1.addOption("Rainbow", AnimationType.Rainbow);
+    ledModeChooser1.addOption("SolidBlue", AnimationType.SolidBlue);
+    ledModeChooser1.addOption("SolidRed", AnimationType.SolidRed);
+
+    SmartDashboard.putData("Current State", ledModeChooser1);
+  }
+  public void setLedMode(AnimationType newMode) {
+    if (state1 != newMode) {
+        state1 = newMode; 
+        switch(state1) {
+            default:
+            case Rainbow:
+                candle.setControl(new RainbowAnimation(0, 7).withSlot(0));
+                
+            case SolidBlue:
+                candle.setControl(new SolidColor(0, 7).withColor(kBlue));
+
+            case SolidRed:
+                candle.setControl(new SolidColor(0, 7).withColor(kRed));
+
+            case None:
+                candle.setControl(new SolidColor(0, 7).withColor(new RGBWColor(0, 0, 0, 0)));
+
+        }
+    }
+    
+}
+
+
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
+    
+    AnimationType selectedMode = ledModeChooser1.getSelected();
+
+    if (state1 != selectedMode) {
+        setLedMode(selectedMode);
+    }
+    
+    
+    
+  }
+
+  /** This function is called once each time the robot enters Disabled mode. */
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      CommandScheduler.getInstance().schedule(m_autonomousCommand);
+    }
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+    setLedMode(AnimationType.Rainbow);
+  }
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
+    
+  }
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
+}
